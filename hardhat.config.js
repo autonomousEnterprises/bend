5f772cfdc0a27dde1c5066d287567039fc5047bc
equaliser0 <accounts@equaliser.minimalcity.de>
2021-12-25 21:06:04 +0100

    Preparing Avalanche Network

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,14 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
+const FORK_FUJI = true
+const FORK_MAINNET = false
+const forkingData = FORK_FUJI ? {
+  url: 'https://api.avax-test.network/ext/bc/C/rpc',
+} : FORK_MAINNET ? {
+  url: 'https://api.avax.network/ext/bc/C/rpc'
+} : undefined
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -24,8 +32,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.6.12",
   networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      networkId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
     localhost: {
-      url: 'http://127.0.0.1:8545/',
+      url: 'http://127.0.0.1:8545/ext/bc/C/rpc',
       accounts:
         ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
     },
@@ -34,6 +47,18 @@ module.exports = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: []
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: []
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
