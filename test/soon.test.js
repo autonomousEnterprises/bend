9865dc3cd434cf278cb6bbd406d9578612966d68
equaliser0 <accounts@equaliser.minimalcity.de>
2021-12-25 21:06:19 +0100

    update Test for Deployment

--- test/soon.test.js
+++ test/soon.test.js
@@ -2,18 +2,11 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Soon", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Should be deployed", async function () {
     const Soon = await ethers.getContractFactory("Soon");
     const soon = await Soon.deploy();
     await soon.deployed();
 
-    expect(await soon.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await soon.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await soon.greet()).to.equal();
+    expect(await soon.symbol()).to.equal("SOON");
   });
 });
