f322d29e15465a0e480f2d71af8c3206326cff9d
equaliser0 <accounts@equaliser.minimalcity.de>
2022-03-07 11:32:01 +0100

    update parameter

--- contracts/Bend.sol
+++ contracts/Bend.sol
@@ -1690,7 +1690,7 @@ contract Bend is Context, ERC20Deflationary {
     string private name_ = "Bend";
     string private symbol_ = "BEND";
     uint8 private decimal_ = 3;
-    uint256 private tokenSupply_ = 10 ** 12;
+    uint256 private tokenSupply_ = 12 * 10 ** 9  ;
     uint8 private taxBurn_ = 2;
     uint8 private taxReward_ = 2;
     uint8 private taxLiquify_ = 2;
@@ -1698,7 +1698,7 @@ contract Bend is Context, ERC20Deflationary {
     uint256 private minTokensBeforeSwap_ = (10 ** 6) * (10 ** decimal_);
     // Testnet Router 0x2D99ABD9008Dc933ff5c0CD271B88309593aB921
     // Mainnet Router 0x60aE616a2155Ee3d9A68541Ba4544862310933d4
-    address private routerAddress = 0x2D99ABD9008Dc933ff5c0CD271B88309593aB921;
+    address private routerAddress = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
 
     constructor () ERC20Deflationary(name_, symbol_, decimal_, tokenSupply_) {
         enableAutoBurn(taxBurn_, taxDecimals_);
